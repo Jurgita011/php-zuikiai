0fcb95c0127d3585205b59e590106214b09b9325
Jurgita <jurgita011@gmail.com>
2023-05-09 20:52:54 +0300

    react

---
+++ r1/src/App.jsx
@@ -0,0 +1,20 @@
+/*import logo from './logo.svg';*/
+import './App.css';
+import Racoon from './Components/004/Racoon';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+       <Racoon/>
+
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
+
+/*  <Racoon></Racoon> galima ir taip rasyti bet falima ir suprumtintai ( <Racoon/>) */
---
+++ r1/src/Components/004/Racoon.jsx
@@ -0,0 +1,11 @@
+function Racoon(){
+    return (
+        <div> 
+           <span className="blue">small</span>
+             <h2 className="red">R-A-C-<i className="blue">00</i>-N</h2>
+        </div>
+    );
+}
+
+export default Racoon;
+/* galima naudoti <> tuscia tada ir grazinama taga </> */
